d5c18eceffac1125350f3b71056227b0aa573506
Natalia Tajuelo <tajuelonatalia@gmail.com>
2024-10-18 10:48:30 +0000

    hecho

--- src/app.py
+++ src/app.py
@@ -7,11 +7,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 url = "https://ycharts.com/companies/TSLA/revenues"
+html_info = requests.get(url).text
 
-headers = 
-response = requests.get(url, headers=headers).text
+if "403 Forbidden" in html_info:
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+    response = requests.get(url, headers=headers)
+    html_info = response.text
 
+soup = BeautifulSoup(html_info,"html.parser")
+print(soup)
 
-
-soup = BeautifulSoup(response,"html.parser")
-soup
+tables = soup.find_all("table")
---
+++ src/app2.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+
+service = Service('ruta/a/chromedriver')  # Especifica la ruta a tu chromedriver
+driver = webdriver.Chrome(service=service)
+
+url = "https://ycharts.com/companies/TSLA/revenues"
+driver.get(url)
+
+table = driver.find_element(By.TAG_NAME, 'table')
+rows = table.find_elements(By.TAG_NAME, 'tr')
+
+data = []
+for row in rows:
+    cols = row.find_elements(By.TAG_NAME, 'td')
+    cols = [col.text for col in cols]
+    data.append(cols)
+
+driver.quit()
+
+df = pd.DataFrame(data, columns=['Date', 'Revenue'])
+print(df)
