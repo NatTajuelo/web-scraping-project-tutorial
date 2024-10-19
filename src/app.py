8edb39adf4691b4fd65e0fc9a53f5bd3b8c0e75b
Natalia Tajuelo <tajuelonatalia@gmail.com>
2024-10-19 09:57:37 +0000

    hecho

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-import os
 import sqlite3
 import time
 from bs4 import BeautifulSoup
@@ -7,41 +6,35 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+#Importar el URL 
 url = "https://ycharts.com/companies/TSLA/revenues"
 html_info = requests.get(url, time.sleep(10)).text
 
-if "403 Forbidden" in html_info:
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
-    response = requests.get(url, headers=headers)
-    time.sleep(10)
-    html_info = response.text
+#Descarga del HTML
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
+response = requests.get(url, headers=headers)
+html_info = response.text
 
+#Transformación del HTML
 soup = BeautifulSoup(html_info,"html.parser")
 print(soup)
 
+#Buscar y trabajar con las tablas (creación de la base de datos)
 tables = soup.find_all("table")
 
+dates = [row.find_all("td")[0].text.strip() for table in tables for row in table.find("tbody").find_all("tr")]
+values = [row.find_all("td")[1].text.strip() for table in tables for row in table.find("tbody").find_all("tr")]
 
-for index, table in enumerate(tables):
-    if ("Tesla Quarterly Revenue" in str(table)):
-        table_index = index
-        break
+df = pd.DataFrame({"Date": dates, "Revenue": values})
 
-#Marco de datos
+df = df.replace({",": "", "B": ""}, regex=True).dropna()
+pd.set_option("display.max_columns", None)
 
-tesla_revenue = pd.DataFrame(columns = ["Date", "Revenue"])
-for row in tables[table_index].tbody.find_all("tr"):
-    col = row.find_all("td")
-    if (col != []):
-        Date = col[0].text
-        Revenue = col[1].text.replace("$", "").replace(",", "")
-        tesla_revenue = pd.concat([tesla_revenue, pd.DataFrame({
-            "Date": Date,
-            "Revenue": Revenue
-        }, index = [0])], ignore_index = True)
+df_filtro = df[df["Date"].str.contains(r'\w{3,9}\s\d{2}\s\d{4}', regex=True)]
+print(df_filtro)
 
-tesla_revenue = tesla_revenue[tesla_revenue["Revenue"] != ""]
-tesla_revenue.head()
+df_filtro = df_filtro[df_filtro["Revenue"] != ""]
+df_filtro.head()
 
 # Almacenar en SQLite
 
@@ -50,7 +43,7 @@ connection = sqlite3.connect("Tesla.db")
 cursor = connection.cursor()
 cursor.execute("""CREATE TABLE revenue (Date, Revenue)""")
 
-tesla_tuples = list(tesla_revenue.to_records(index = False))
+tesla_tuples = list(df_filtro.to_records(index = False))
 tesla_tuples[:5]
 
 cursor.executemany("INSERT INTO revenue VALUES (?,?)", tesla_tuples)
@@ -63,9 +56,10 @@ for row in cursor.execute("SELECT * FROM revenue"):
 
 fig, axis = plt.subplots(figsize = (10, 5))
 
-tesla_revenue["Date"] = pd.to_datetime(tesla_revenue["Date"])
-tesla_revenue["Revenue"] = tesla_revenue["Revenue"].astype('int')
-sns.lineplot(data = tesla_revenue, x = "Date", y = "Revenue")
+df_filtro["Date"] = pd.to_datetime(df_filtro["Date"])
+df_filtro["Revenue"] = pd.to_numeric(df_filtro["Revenue"], errors='coerce')
+df_filtro["Revenue"] = df_filtro["Revenue"].fillna(0).astype(int)
+sns.lineplot(data = df_filtro, x = "Date", y = "Revenue")
 
 plt.tight_layout()
 
@@ -75,10 +69,10 @@ plt.show()
 
 fig, axis = plt.subplots(figsize = (10, 5))
 
-tesla_revenue["Date"] = pd.to_datetime(tesla_revenue["Date"])
-tesla_revenue_yearly = tesla_revenue.groupby(tesla_revenue["Date"].dt.year).sum().reset_index()
+df_filtro["Date"] = pd.to_datetime(df_filtro["Date"])
+tesla_revenue_yearly = df_filtro.groupby(df_filtro["Date"].dt.year)["Revenue"].sum().reset_index()
 
-sns.barplot(data = tesla_revenue_yearly[tesla_revenue_yearly["Date"] < 2023], x = "Date", y = "Revenue")
+sns.barplot(data = tesla_revenue_yearly[tesla_revenue_yearly["Date"] < 2023], x = "Date", y = "Revenue", palette="flare")
 
 plt.tight_layout()
 
@@ -88,10 +82,12 @@ plt.show()
 
 fig, axis = plt.subplots(figsize = (10, 5))
 
-tesla_revenue_monthly = tesla_revenue.groupby(tesla_revenue["Date"].dt.month).sum().reset_index()
+tesla_revenue_monthly = df_filtro.groupby(df_filtro["Date"].dt.month)["Revenue"].sum().reset_index()
 
-sns.barplot(data = tesla_revenue_monthly, x = "Date", y = "Revenue")
+sns.barplot(data = tesla_revenue_monthly, x = "Date", y = "Revenue", palette="light:#5A9")
 
 plt.tight_layout()
 
 plt.show()
+
+# En el archivo explore.es.ipynb se pueden ver los gráficos #
